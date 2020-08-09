298b15cff11c8b01b52b5e86c62b5188e4e5c154
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2020-08-09 12:44:19 +0200

    example prepared

--- main.tf
+++ main.tf
@@ -1,44 +1,11 @@
-# Configure the AWS Provider
 provider "aws" {
 }
 
-# Create a VPC
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_instance" "example" {
-  ami      = "ami-0d359437d1756caa8"
-  instance_type = "t2.micro"
 }
 
-resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
-}
-
-resource "aws_key_pair" "example" {
-  key_name   = "examplekey"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
-resource "aws_instance" "example2" {
-  key_name      = aws_key_pair.example.key_name
-  ami           = "ami-0c115dbd34c69a004"
-  instance_type = "t2.micro"
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("~/.ssh/id_rsa")
-    host        = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo amazon-linux-extras enable nginx1.12",
-      "sudo yum -y install nginx",
-      "sudo systemctl start nginx"
-    ]
-  }
+resource "aws_s3_bucket" "example" {
 }
